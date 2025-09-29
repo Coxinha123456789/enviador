ef8cf48da653f25543234488cdbb940eb52e4428
Thales Sena Santos <thalessena272006@gmail.com>
2025-09-29 18:41:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def analyze_image_with_gemini(image_bytes):
         model = genai.GenerativeModel(model_name='gemini-2.5-flash')
         
         image_pil = Image.open(io.BytesIO(image_bytes))
-        prompt = "Descreva detalhadamente o que você vê nesta imagem, de forma objetiva. Esta descrição será enviada em um e-mail para um supervisor, para que ele entenda o conteúdo da imagem sem precisar abri-la."
+        prompt = "Descreva o que você vê nesta imagem, de forma objetiva, com mais ou menos um paragrafo."
         
         response = model.generate_content([prompt, image_pil])
         return response.text
