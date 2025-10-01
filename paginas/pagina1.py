ad4e72d5aa1565806cfbf254b57285d2aaf2000e
Thales Sena Santos <thalessena272006@gmail.com>
2025-09-30 21:35:25 -0300

    Update pagina1.py

--- paginas/pagina1.py
+++ paginas/pagina1.py
@@ -17,17 +17,25 @@ from firebase_admin import credentials, firestore, storage
 
 @st.cache_resource
 def conectar_firebase():
+    """Inicializa o Firebase e retorna os clientes do Firestore e do Storage."""
     try:
         firebase_admin.get_app()
     except ValueError:
         cred = credentials.Certificate(dict(st.secrets["firebase"]))
-        firebase_admin.initialize_app(cred, {
-            "storageBucket": "bancoenviados.firebasestorage.app"  # 🔹 seu bucket aqui
-        })
-    return firestore.client()
+        firebase_admin.initialize_app(
+            cred, 
+            {
+                'storageBucket': st.secrets.firebase.storageBucket
+            }
+        )
+    
+    db = firestore.client()
+    bucket = storage.bucket()
+    return db, bucket
 
 
-db = conectar_firebase()
+db, bucket = conectar_firebase()
+
 colecao = 'ColecaoEnviados'
 
 # --- Carregamento de Segredos ---
