0de63ef9686f99625d6e4e0b97f7b635c594f4bc
Thales Sena Santos <thalessena272006@gmail.com>
2025-10-06 19:54:00 -0300

    Update dashboard.py

--- paginas/dashboard.py
+++ paginas/dashboard.py
@@ -84,7 +84,7 @@ with col1:
     })
     
     status_df['color'] = status_df['status'].map(color_map)
-    st.bar_chart(status_df.set_index('status'), y='counts', color='color')
+    st.bar_chart(status_df.set_index('status'), y='count', color='color')
 
 with col2:
     st.subheader("Envios por Colaborador")
